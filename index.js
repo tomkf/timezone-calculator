f087790e9d36902afe03dbae6b85d6b32ee63c62
Thomas Ferris <thomaskeithferris@gmail.com>
2019-10-21 16:56:33 -0700

    valid passing from api

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ const config = require("./config.js").tokens;
 
 const timeModule = require("./timeCal");
 
-// console.log(timeModule.timeCalc("2019-10-21T17:17:13", "2019-10-21T14:17:13"));
-
-//test examples:
-//2019-10-21T17:17:13
-//2019-10-21T14:17:13
+let apiDatabase = [];
 
 app.use(express.static("views"));
 app.use(express.static("styles"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-let apiDatabase = [];
 
 const requestForecast = userCity => {
   return new Promise((resolve, reject) => {
@@ -30,11 +24,6 @@ const requestForecast = userCity => {
     }`;
     request(mapApiCall, function(error, response, body) {
       let responce = JSON.parse(body);
-      // console.log(responce);
-      // console.log("*********************************************");
-      // console.log("*********************************************");
-      console.log(responce.weather[0].description);
-      apiDatabase.push(responce.weather[0].description);
       resolve(responce.weather[0].description);
     });
   });
@@ -48,71 +37,56 @@ const requestTimeZone = userCity => {
 
     request(zoneApiCall, function(error, response, body) {
       let responce = JSON.parse(body);
-      console.log(responce.resourceSets[0].resources[0].timeZoneAtLocation[0]);
-      console.log(
-        responce.resourceSets[0].resources[0].timeZoneAtLocation[0].timeZone[0]
-          .convertedTime
+      // console.log(
+      //   responce.resourceSets[0].resources[0].timeZoneAtLocation[0].timeZone[0]
+      //     .convertedTime.localTime
+      // );
+      // console.log(
+      //   responce.resourceSets[0].resources[0].timeZoneAtLocation[0].timeZone[0]
+      //     .convertedTime
+      // );
+      resolve(
+        apiDatabase.push({
+          name:
+            responce.resourceSets[0].resources[0].timeZoneAtLocation[0]
+              .placeName,
+          timeVal:
+            responce.resourceSets[0].resources[0].timeZoneAtLocation[0]
+              .timeZone[0].convertedTime.localTime,
+          zoneName:
+            responce.resourceSets[0].resources[0].timeZoneAtLocation[0]
+              .timeZone[0].genericName,
+          zoneAbv:
+            responce.resourceSets[0].resources[0].timeZoneAtLocation[0]
+              .timeZone[0].abbreviation
+        })
       );
-      return responce
-        .resourceSets[0].resources[0].timeZoneAtLocation[0].timeZone[0].convertedTime;
     });
   });
 };
 
-// const requestCalls = requestObj => {
-//   //requestObj will be an array built  from the event listenrs in the front-end...
-
-//   request(mapApiCall, function(error, response, body) {
-//     //console.log("error:", error); // Print the error if one occurred
-//     let responce = JSON.parse(body);
-//     // console.log(responce.coord.lon);
-//     // console.log(responce.coord.lat);
-
-//     //   let zoneApiCall = `http://api.timezonedb.com/v2.1/get-time-zone?key=${
-//     //     config[1]
-//     //   }&format=json&by=position&lat=${responce.coord.lat}&lng=${
-//     //     responce.coord.lon
-//     //   }
-//     // `;
-
-//     //   request(zoneApiCall, function(error, response, body) {
-//     //     let responceB = JSON.parse(body);
-//     //     console.log(responceB);
-//     //   });
-
-//     let bingAPiCall = `https://dev.virtualearth.net/REST/v1/TimeZone/query=${
-//       responce.name
-//     }?key=${config[1]}`;
-
-//     request(bingAPiCall, function(error, response, body) {
-//       let responceB = JSON.parse(body);
-//       console.log(responceB.resourceSets[0].resources[0].timeZoneAtLocation[0]);
-//       console.log(
-//         responceB.resourceSets[0].resources[0].timeZoneAtLocation[0].timeZone[0]
-//           .convertedTime
-//       );
-//     });
-
-//     let results = responce.weather[0];
-//     console.log(response.coord);
-//   });
-// };
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.post("/display", async function(req, res) {
-  forecastA = await requestForecast(req.body.cityA);
+  let forecastA = await requestForecast(req.body.cityA);
 
-  forecastB = await requestForecast(req.body.cityB);
+  let forecastB = await requestForecast(req.body.cityB);
 
-  timeZoneA = await requestTimeZone(req.body.cityA);
+  await requestTimeZone(req.body.cityA);
 
-  timeZoneB = await requestTimeZone(req.body.cityB);
+  await requestTimeZone(req.body.cityB);
+
+  let objA = apiDatabase[0];
+
+  let objB = apiDatabase[1];
+
+  // let timeCall = timeModule.timeCalc();
+  console.log("test:", apiDatabase);
 
   let nameA = req.body.cityA;
   let nameB = req.body.cityB;
-  console.log(forecastA, forecastB);
-  let template = { nameA, nameB, forecastA, forecastB };
+
+  let template = { nameA, nameB, forecastA, forecastB, objA, objB };
   res.render("display.ejs", template);
 });
 
